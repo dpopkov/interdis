fb3e41ddc5619edebdd2f58c190df33fd48ea7fd
Denis Popkov <pkvdenis@gmail.com>
2019-12-03 18:13:07 +0300

    ch11: UseThree

---
+++ interdisjv/src/main/java/org/daydevjv/interdisjv/ch11/UseThree.java
@@ -0,0 +1,9 @@
+package org.daydevjv.interdisjv.ch11;
+
+public class UseThree {
+    public static void main(String[] args) {
+        System.out.print(args[2] + " ");
+        System.out.print(args[1] + " ");
+        System.out.println(args[0]);
+    }
+}
---
+++ interdispy/ch11/usethree.py
@@ -0,0 +1,6 @@
+import sys
+import stdio
+
+stdio.write(sys.argv[3] + ' ')
+stdio.write(sys.argv[2] + ' ')
+stdio.writeln(sys.argv[1])
