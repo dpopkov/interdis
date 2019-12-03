0ca3de1c5d84fdf6a15a3c3faddaa9ea5c4dfaad
Denis Popkov <pkvdenis@gmail.com>
2019-12-03 17:48:40 +0300

    add java project interdisjv

---
+++ interdisjv/src/test/java/org/daydevjv/interdisjv/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package org.daydevjv.interdisjv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
