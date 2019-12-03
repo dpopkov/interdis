27ea346d01303d9a3e44a3670486a2ec399f56ca
Denis Popkov <pkvdenis@gmail.com>
2019-12-03 19:02:43 +0300

    ch12: Ruler - string concatenation

---
+++ interdispy/ch12/ruler.py
@@ -0,0 +1,11 @@
+import stdio
+
+# Программа 1.2.1. Пример конкатенации строк
+ruler1 = '1'
+ruler2 = ruler1 + ' 2 ' + ruler1
+ruler3 = ruler2 + ' 3 ' + ruler2
+ruler4 = ruler3 + ' 4 ' + ruler3
+stdio.writeln(ruler1)
+stdio.writeln(ruler2)
+stdio.writeln(ruler3)
+stdio.writeln(ruler4)
