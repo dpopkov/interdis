da77b240651f196f5b900f4ae4553b4f04403d7e
Denis Popkov <pkvdenis@gmail.com>
2019-12-06 18:29:52 +0300

    ch12: exercises 20,23,24,34 (finished ch1.2)

--- interdisjv/src/main/java/org/daydevjv/interdisjv/ch12/Exercises12.java
+++ interdisjv/src/main/java/org/daydevjv/interdisjv/ch12/Exercises12.java
@@ -81,6 +81,81 @@ public class Exercises12 {
         return from + random.nextInt(to - from + 1);
     }
 
+    // ex1_2_17 in py
+    private static void ex1_2_20() {
+        System.out.println();
+        Random random = new Random();
+        int a = random.nextInt(5) + 1;
+        int b = random.nextInt(5) + 1;
+        System.out.println(a + b);
+    }
+
+    // ex1_2_20 in py
+    private static void ex1_2_23() {
+        System.out.println();
+        System.out.println(withinInterval(19, 3));
+        System.out.println(withinInterval(20, 3));
+        System.out.println(withinInterval(21, 3));
+        System.out.println(withinInterval(15, 5));
+        System.out.println(withinInterval(20, 6));
+        System.out.println(withinInterval(19, 6));
+        System.out.println(withinInterval(21, 6));
+    }
+
+    private static boolean withinInterval(int day, int month) {
+        final int startDay = 20;
+        final int startMonth = 3;
+        final int endDay = 20;
+        final int endMonth = 6;
+        boolean out = false;
+        if (month < startMonth || month > endMonth) {
+            out = true;
+        } else if (month == startMonth) {
+            out = day < startDay;
+        } else if (month == endMonth) {
+            out = day > endDay;
+        }
+        return !out;
+    }
+
+    private static void ex1_2_24() {
+        System.out.println();
+        System.out.println(continuouslyCompoundedInterest(10_000, 3, 0.05));
+        System.out.println(continuouslyCompoundedInterest(20_000, 10, 0.15));
+    }
+
+    private static double continuouslyCompoundedInterest(double investment, int years, double annualInterestRate) {
+        return investment * Math.pow(Math.E, annualInterestRate * years);
+    }
+
+    // ex1_2_31 in py
+    private static void ex1_2_34() {
+        System.out.println();
+        threeSort(10, 30, 20);
+        threeSort(30, 20, 10);
+        threeSort(20, 10, 30);
+    }
+
+    private static void threeSort(int a, int b, int c) {
+        int tmp;
+        if (b < a) {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (c < a) {
+            tmp = a;
+            a = c;
+            c = tmp;
+        }
+        if (c < b) {
+            tmp = b;
+            b = c;
+            c = tmp;
+        }
+        System.out.println(a + " " + b + " " + c);
+    }
+
     public static void main(String[] args) {
         ex1_2_1();
         ex1_2_2();
@@ -88,5 +163,9 @@ public class Exercises12 {
         ex1_2_14();
         ex1_2_15();
         ex1_2_19();
+        ex1_2_20();
+        ex1_2_23();
+        ex1_2_24();
+        ex1_2_34();
     }
 }
--- interdispy/ch12/exercises12.py
+++ interdispy/ch12/exercises12.py
@@ -72,9 +72,82 @@ def ex1_2_16():
     print(random.randint(-1, 1))
 
 
+# ex1_2_20 in java
+def ex1_2_17():
+    print()
+    a = random.randint(1, 6)
+    b = random.randint(1, 6)
+    print(a + b)
+
+
+# ex1_2_23 in java
+def ex1_2_20():
+    print()
+    print(within_interval(19, 3))
+    print(within_interval(20, 3))
+    print(within_interval(21, 3))
+    print(within_interval(15, 5))
+    print(within_interval(20, 6))
+    print(within_interval(19, 6))
+    print(within_interval(21, 6))
+
+
+def within_interval(day, month):
+    start_day = 20
+    start_month = 3
+    end_day = 20
+    end_month = 6
+    out = False
+    if month < start_month or end_month < month:
+        out = True
+    elif month == start_month:
+        out = day < start_day
+    elif month == end_month:
+        out = day > end_day
+    return not out
+
+
+def ex1_2_21():
+    print()
+    print(continuously_compounded_interest(10000, 3, 0.05))
+    print(continuously_compounded_interest(20000, 10, 0.15))
+
+
+def continuously_compounded_interest(investment, years, annual_rate):
+    return investment * (math.e ** (annual_rate * years))
+
+
+# ex1_2_34 in java
+def ex1_2_31():
+    print()
+    three_sort(10, 30, 20)
+    three_sort(30, 20, 10)
+    three_sort(20, 10, 30)
+
+
+def three_sort(a, b, c):
+    if b < a:
+        t = a
+        a = b
+        b = t
+    if c < a:
+        t = a
+        a = c
+        c = t
+    if c < b:
+        t = b
+        b = c
+        c = t
+    print(a, b, c, ' ')
+
+
 ex1_2_1()
 ex1_2_2()
 ex1_2_3()
 ex1_2_11()
 ex1_2_12()
 ex1_2_16()
+ex1_2_17()
+ex1_2_20()
+ex1_2_21()
+ex1_2_31()
